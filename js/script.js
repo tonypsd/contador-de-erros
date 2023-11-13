2022bd3d9c3c6cf9faf9e87afbd84498e0431078
tonypsd <150407470+tonypsd@users.noreply.github.com>
2023-11-12 21:15:07 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,18 +2,24 @@ document.addEventListener('DOMContentLoaded', function () {
     var contadorBtn = document.getElementById('contadorBtn');
     var contadorBtn2 = document.getElementById('contadorBtn2');
   
-    var contador = 0;
+    var contadorDesign = 0;
+    var contadorTexto = 0;
   
-    function atualizarContador(botao) {
-      contador++;
-      botao.textContent = contador;
+    function atualizarContadorDesign(botao) {
+      contadorDesign++;
+      botao.textContent = contadorDesign;
+    }
+    
+    function atualizarContadorTexto(botao) {
+      contadorTexto++;
+      botao.textContent = contadorTexto;
     }
   
     contadorBtn.addEventListener('click', function () {
-      atualizarContador(contadorBtn);
+      atualizarContadorDesign(contadorBtn);
     });
   
     contadorBtn2.addEventListener('click', function () {
-      atualizarContador(contadorBtn2);
+      atualizarContadorTexto(contadorBtn2);
     });
   });
