38f860e2ad0e7c22fb44aa7ce68c10e31e95cf87
tonypsd <150407470+tonypsd@users.noreply.github.com>
2023-11-17 00:09:25 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var contadorBtn = document.getElementById('contadorBtn');
-    var contadorBtn2 = document.getElementById('contadorBtn2');
+  var contadorBtn = document.getElementById('contadorBtn');
+  var contadorBtn2 = document.getElementById('contadorBtn2');
+  var imagem = document.getElementById('imagemTroca');
+
+  var contadorDesign = 0;
+  var contadorTexto = 0;
+
+  function atualizarContadorDesign(botao) {
+    contadorDesign++;
+    botao.textContent = contadorDesign;
+    mudarImagem('./img/mont.png');
+  }
   
-    var contadorDesign = 0;
-    var contadorTexto = 0;
-  
-    function atualizarContadorDesign(botao) {
-      contadorDesign++;
-      botao.textContent = contadorDesign;
-    }
-    
-    function atualizarContadorTexto(botao) {
-      contadorTexto++;
-      botao.textContent = contadorTexto;
-    }
-  
-    contadorBtn.addEventListener('click', function () {
-      atualizarContadorDesign(contadorBtn);
-    });
-  
-    contadorBtn2.addEventListener('click', function () {
-      atualizarContadorTexto(contadorBtn2);
-    });
+  function atualizarContadorTexto(botao) {
+    contadorTexto++;
+    botao.textContent = contadorTexto;
+    mudarImagem('./img/mont2.png');
+  }
+
+  contadorBtn.addEventListener('click', function () {
+    atualizarContadorDesign(contadorBtn);
   });
+
+  contadorBtn2.addEventListener('click', function () {
+    atualizarContadorTexto(contadorBtn2);
+  });
+
+  function mudarImagem(novaSrc) {
+    imagem.src = novaSrc;
+  }
+});
